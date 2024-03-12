4db9a3b6ec96feaab89793b008ab75b8189eb744
zchflyer <zchflyer@gmail.com>
2024-03-12 11:24:52 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your_package_name",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # 任何依赖项
+    ],
+    # 其他元数据
+)
